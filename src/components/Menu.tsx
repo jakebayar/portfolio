4cf078e06e322e96833d7da551e207e979e3fc8f
Jake Bayar <20175302+jakebayar@users.noreply.github.com>
2023-11-22 14:04:37 -0500

    needs icons for menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from './ui/button'
+import { HomeIcon } from '@heroicons/react/24/solid'
+
 
 
 const options = [
-    {   
+    {
         route: 'home',
-        icon: '',
+        icon: 'HomeIcon',
         alt: 'home icon',
 
     },
@@ -49,7 +51,7 @@ export default function Menu() {
         const buttons = [];
         for (let i = 0; i < 8; i++) {
             buttons.push(
-                <Button key={i} variant="outline" className='w-11 h-11 rounded-2xl text-xs gap-1.5'>
+                <Button key={i} variant="outline" className='w-11 h-11 rounded-2xl text-xs'>
                     <Image src={'/arrow-icon.svg'} width={30} height={30} alt='arrow icon' />
                 </Button>
             );
@@ -58,7 +60,7 @@ export default function Menu() {
     };
 
     return (
-        <div className="flex flex-row overflow-x-auto no-scrollbar backdrop-blur shadow-lg rounded-3xl bg-slate-300/80 p-2 mx-16 my-4 md:my-8">
+        <div className="flex flex-row overflow-x-auto no-scrollbar backdrop-blur shadow-xl rounded-3xl bg-slate-300/80 p-2 mx-16 my-4 md:my-8">
 
             <div className='flex flex-row rounded-2xl gap-1 overflow-x-auto no-scrollbar'>
                 <div className='flex flex-row gap-1'>
